9a9b5f0aaea43264edc4e97cf152236a613e4506
BSravya <sravyabutharaju123@gmail.com>
2023-01-15 21:53:12 +0530

    App Name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Osprey Adventure
         </a>
       </header>
     </div>
